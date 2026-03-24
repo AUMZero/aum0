00cf1d362022f904e6c5afd063b2461ec084d55c
AUMZero <256117066+AUMZero@users.noreply.github.com>
2026-03-24 13:22:37 +0900

    src: state layout, IERC20

--- src/AUM0.sol
+++ src/AUM0.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+interface IERC20 {
+    function transferFrom(address, address, uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
+    function balanceOf(address) external view returns (uint256);
+    function approve(address, uint256) external returns (bool);
+}
+
 contract AUM0 {
-    // sketch: to grow into help-only-invariant asset manager
+    // state and setTarget in progress
+    struct Target {
+        uint32 minDriftBps;
+        uint32 bandBps;
+        uint64 bountyQuote;
+        uint16[] weightsBps;
+    }
+    mapping(address => Target) internal targets;
+    mapping(address => mapping(uint256 => uint256)) public balanceOf;
 }
