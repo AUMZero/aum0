90164cb8c2a3e368f258b6cbca2f6386f5a8c253
AUMZero <256117066+AUMZero@users.noreply.github.com>
2026-05-27 11:08:44 +0900

    test: scaffold + mock erc20/feed

---
+++ test/AUM0.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import "../src/AUM0.sol";
+
+contract MockERC20 { /* to grow */ }
+contract MockFeed  { /* to grow */ }
+
+contract AUM0Test is Test {
+    function testSanity() public {}
+}
