18037a3eb868be16d4d1281138d1581ec77e10a9
AUMZero <256117066+AUMZero@users.noreply.github.com>
2026-07-21 15:50:05 +0900

    test: fork scaffold, hood rpc

---
+++ test/AUM0.fork.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import "../src/AUM0.sol";
+
+contract AUM0ForkTest is Test {
+    function testDeploysOnFork() public {}
+}
