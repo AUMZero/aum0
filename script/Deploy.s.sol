3c2c00d9c9a2f7d7c4fc4fdd0ddfbd34f5c3e483
AUMZero <256117066+AUMZero@users.noreply.github.com>
2026-08-06 13:15:51 +0900

    script: deploy scaffold

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Script.sol";
+import "../src/AUM0.sol";
+
+contract Deploy is Script {
+    function run() external {}
+}
